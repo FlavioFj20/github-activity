ad410aa50cc0ca1659d30ffbb7d2cc70c9bac4ff
FlavioFj18 <fuxiflavio86@gmail.com>
2026-08-09 12:38:51 +0100

    Implement command-line argument parsing and usage validation

--- src/index.ts
+++ src/index.ts
@@ -8,4 +8,13 @@ function main(){
       strict: true,
     })
     console.log(positionals);
+    const [username, ...extra] = positionals;
+    if (extra.length > 0){
+      console.error(`Wrong Usage:\n\tExtra arguments detected: ${extra.join(',')}`);
+      console.error(`Correct Usage:\n\t"github-activity <username>"`);
+      process.exit(1);
+    }
+    console.log(`Hello ${username}`);
 }
+
+main();
